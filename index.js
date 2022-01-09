c77f980d80e441725ed4c896e9226bd45ff84a03
TaronReactDev <taronsargsyan.reactdev@gmail.com>
2022-01-09 21:02:54 +0400

    3

--- index.js
+++ index.js
@@ -1,59 +1,86 @@
-const array = [1,[2,3,[4]],[[5,6],7]];
-let flatArray = [];
-function flat(arr){
-   
-    arr.map(el => {
+let obj = {
+  num: 1,
+  number: 2,
+  nam3: 3,
+  obj2: {
+    a: 2,
+    s: 5,
+    obj3: {
+      a: 5,
+      s: 55,
+    },
+  },
+};
 
-        if(!Array.isArray(el)){
+function revers(obj) {
+  let arr = Object.entries(obj).map((el) => {
+    if (typeof el[1] == "object") {
+      return [el[0], revers(el[1])];
+    } else {
+      return el.reverse();
+    }
+  });
 
-         flatArray.push(el)
-
-        } else{
-
-            flat(el)
-        }
-    })
-  
-}
- flat(array)
-
-
-
-
- //map
- function map(arr, cb){
-    let newArr = [];
-    for(let i = 0; i< arr.length; i++){
-       newArr.push(cb(arr[i], i, arr)) 
-    }  
-    return newArr
-    
- }
-let result = map([2,3], (el)=>{return(el)})
-
-
-//slice
-function slice(arr, start = 0, end = arr.length-1, ...rest){
-    let newSliceArr = [];
-
-   if(end < 0 ){
-       end = arr.length+end
-} 
-if( start < 0 && start <end){
-    start=arr.length+start
+  return Object.fromEntries(arr);
 }
 
+console.log(revers(obj));
 
+///////////////////////////////////////////////////////////////////////
+// const array = [1,[2,3,[4]],[[5,6],7]];
+// let flatArray = [];
+// function flat(arr){
 
-    for(let i = start ; i<end; i++){
-        newSliceArr.push(arr[i])
-      }
-   
-      newSliceArr = [...newSliceArr, ...rest]
+//     arr.map(el => {
 
-return newSliceArr
- }
+//         if(!Array.isArray(el)){
 
+//          flatArray.push(el)
 
- let resultSlice = slice([5,6,4,5,55,2,54,5441,4,5,55,2,54,5441], 2,5, 11,11);
+//         } else{
 
+//             flat(el)
+//         }
+//     })
+
+// }
+//  flat(array)
+
+//  //map
+//  function map(arr, cb){
+//     let newArr = [];
+//     for(let i = 0; i< arr.length; i++){
+//        newArr.push(cb(arr[i], i, arr))
+//     }
+//     return newArr
+
+//  }
+// let result = map([2,3], (el)=>{return(el)})
+
+// //slice
+// function slice(arr, start = 0, end = arr.length-1, ...rest){
+//     let newSliceArr = [];
+
+//    if(end < 0 ){
+//        end = arr.length+end
+// }
+// if( start < 0 && start <end){
+//     start=arr.length+start
+// }
+
+//     for(let i = start ; i<end; i++){
+//         newSliceArr.push(arr[i])
+//       }
+
+//       newSliceArr = [...newSliceArr, ...rest]
+
+// return newSliceArr
+//  }
+
+//  let resultSlice = slice([5,6,4,5,55,2,54,5441,4,5,55,2,54,5441], 2,-5);
+// console.log(resultSlice);
+
+// let a =[5,6,4,5,55,2,54,5441,4,5,55,2,54,5441]
+// let b = a.slice(-2,5)
+
+// console.log("b", b);
